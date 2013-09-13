e0b689cbb21fd79caf9984707d0d0022cce6edcb
Bill Mill <bill.mill@gmail.com>
2013-09-12 22:07:35 -0400

    properly handle wide sprites

--- split.py
+++ split.py
@@ -6,6 +6,7 @@ positions = re.findall("\.flair-(.*?)[,{].*?background-position:0 -(\d+)", fin.r
 prev = 0
 current_image = 1
 crests = Image.open('%s.png' % current_image)
+width = 20
 for team, offset in positions:
     #if the team name ends in -s2 -s3 -s4, chop it
     if re.search('-s\d', team):
@@ -17,8 +18,12 @@ for team, offset in positions:
     if offset != prev + 21:
         current_image += 1
         crests = Image.open('%s.png' % current_image)
+        if current_image == 3:
+            width = 40
+        else:
+            width = 20
 
     prev = offset
 
-    crest = crests.crop((0, offset, 20, offset + 20))
+    crest = crests.crop((0, offset, width, offset + 20))
     crest.save('crests/%s.png' % team)
