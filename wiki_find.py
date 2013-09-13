7428e9dde66bc1a50535310e3beb4502af99d303
Bill Mill <bill.mill@gmail.com>
2013-09-13 15:28:24 -0400

    working on wikifind

--- wiki_find.py
+++ wiki_find.py
@@ -2,32 +2,29 @@ import wikipedia
 import requests
 import re
 import os, os.path
+import codecs
 
 def logo_guess(team, guessword="football"):
     p = wikipedia.page(wikipedia.search("%s %s" % (team, guessword))[0])
     print "found page %s" % p.title
+
     files = re.findall('<table[^\n]*?vcard.*?a href="(/wiki/File.*?)" class="image".*?</table>', p.html(), re.S)
     imagepage = "http://en.wikipedia.org" + files[0]
+
     r = requests.get(imagepage)
     fullimage = re.findall(r'fullImageLink.*?href="(.*?)".*?/div', r.content, re.S)
     assert fullimage[0].startswith("//")
-    return 'http:' + fullimage[0]
+
+    image = 'http:' + fullimage[0]
+    return (p.title, image)
 
 def download(image_url, image_name):
     cmd = "wget %s -O '%s' > /dev/null 2>&1 &" % (image_url, image_name)
     print cmd
     os.system(cmd)
 
-def exceptions(team):
-    if 'Bor Dort' in team: return 'Borussia Dortmund'
-    if 'monchenglad' in team.lower(): return 'Borussia Monchengladbach'
-    if 'Hamburg' in team: return 'Hamburger SV'
-    if 'nurnberg' in team.lower(): return 'FC Nurnberg'
-    if 'valencia' in team.lower(): return 'Valencia CF'
-    return team
-
 def get(team):
-    team = exceptions(team)
+    wiki_pages = codecs.open("wikinames.txt", 'w', 'utf8')
 
     #if we already have it, bail
     for extension in ["svg", "png"]:
@@ -37,13 +34,18 @@ def get(team):
             return
 
     try:
-        logo_file = logo_guess(team)
+        page, logo_file = logo_guess(team)
     except IndexError:
         try:
-            logo_file = logo_guess(team, "club")
+            page, logo_file = logo_guess(team, "club")
         except IndexError:
-            print "unable to get logo for team %s" % team
-            return
+            try:
+                page, logo_file = logo_guess(team, "club")
+            except IndexError:
+                print "unable to get logo for team %s" % team
+                return
+
+    wiki_pages.write(page + "\n")
 
     extension = re.split('\.', logo_file)[-1]
     filename = "wikicrests/%s.%s" % (team, extension)
@@ -56,6 +58,3 @@ if __name__=="__main__":
             team = team.strip()
             print team
             get(team)
-    #print logo_guess('betis')
-    #print logo_guess('juventus')
-    #print logo_guess('botafogo')
